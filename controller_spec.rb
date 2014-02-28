afad65caad9d3e70b37765f77a72e6e4e5088be8
Sam Livingston-Gray <geeksam@gmail.com>
2014-02-28 11:12:45 -0800

    drop redundant test

--- controller_spec.rb
+++ controller_spec.rb
@@ -1,17 +1,6 @@
 describe MessagesController do
   let(:message) {  "This is a test, yo" }
 
-  it "posts to campfire using Tinder" do
-    campfire = double("Tinder::Campfire")
-    expect(Tinder::Campfire).to receive(:new).and_return(campfire)
-    room = double("Tinder::Campfire room")
-    expect(campfire).to receive(:find_room_by_name).and_return(room)
-    expect(room).to receive(:speak).with(message)
-
-    post :post_message, message: message
-    expect(response).to be_redirect
-  end
-
   it "posts to campfire using CampfirePoster" do
     expect(CampfirePoster).to receive(:post).with(message)
     
